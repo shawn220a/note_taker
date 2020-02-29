c10c1fcd8f2dae05014d66c4dd907e46fe9dd42d
shawn220a <shawn.tschoepe@icloud.com>
2020-02-29 13:29:36 -0600

    Add delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,28 +2,35 @@ const fs = require('fs');
 
 module.exports = (app) => {
 
+  // Display JSON file
   app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', (err, data) => {
       if (err) throw err;
+      
       let notesData = JSON.parse(data);
       res.json(notesData);
     });
   });
 
+  // Write to JSON file
   app.post('/api/notes', (req, res) => {
     let newData = req.body;
+
     fs.readFile('db/db.json', (err, data) => {
       if (err) throw err;
+
       let notesData = JSON.parse(data);
       notesData.push(newData);
       newData = JSON.stringify(notesData);
+
       fs.writeFile('db/db.json', newData, (err) => {
         if (err) throw err;
       });
     });
   });
 
-  // app.delete('/api/notes/:id', (req, res) => {
-
-  // });
+  // Delete JSON file
+  app.delete('/api/notes/:id', (req, res) => {
+    console.log(req);
+  });
 };
